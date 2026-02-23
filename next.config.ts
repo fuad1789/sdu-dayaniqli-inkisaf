db9baaa671412d2210f4830656c6383d953028ae
fuad1789 <fuadbagiyev@gmail.com>
2026-02-23 15:01:27 +0400

    feat: Add Content Security Policy (CSP) headers to Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://static.cloudflareinsights.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://www.googletagmanager.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com;
+`;
 
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
 export default nextConfig;
